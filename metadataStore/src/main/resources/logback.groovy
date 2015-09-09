1d96ea7475dee353b602a72569a89371ddbfea85
John Scancella <jsca@loc.gov>
2015-09-09 09:08:23 -0400

    fixed logging so that it makes the proper directories and log the proper levels

--- metadataStore/src/main/resources/logback.groovy
+++ metadataStore/src/main/resources/logback.groovy
@@ -8,9 +8,34 @@ appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
     pattern = "%date %level [%thread] %logger{10} [%file:%line] %msg%n"
   }
+  filter(ch.qos.logback.classic.filter.ThresholdFilter) {
+    level = INFO
+  }
 }
 
-root(INFO, ["STDOUT"])
+appender("FILEOUT", FileAppender) {
+  append = false
+  file = "${System.getProperty("app.home")}/logs/metadataStore.log"
+  encoder(PatternLayoutEncoder) {
+    pattern = "%date %level [%thread] %logger{10} [%file:%line] %msg%n"
+  }
+  filter(ch.qos.logback.classic.filter.ThresholdFilter) {
+    level = INFO
+  }
+}
+
+appender("DEBUGOUT", FileAppender) {
+  append = false
+  file = "${System.getProperty("app.home")}/logs/debug.log"
+  encoder(PatternLayoutEncoder) {
+    pattern = "%date %level [%thread] %logger{10} [%file:%line] %msg%n"
+  }
+  filter(ch.qos.logback.classic.filter.ThresholdFilter) {
+    level = DEBUG
+  }
+}
+
+root(DEBUG, ["STDOUT", "FILEOUT", "DEBUGOUT"])
 
 //these two are set to ERROR since they output unneeded logging at the info level
 logger("gov.loc.rdc.app.Main", ERROR)
