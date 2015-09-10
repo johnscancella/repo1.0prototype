faeaa5f16bebf619b4fbfd1b06d6200f4890b643
John Scancella <jsca@loc.gov>
2015-09-10 13:47:54 -0400

    fixed port conflict with jenkins for test

--- springWebAppBase/src/test/java/gov/loc/rdc/app/MainApplicationTest.java
+++ springWebAppBase/src/test/java/gov/loc/rdc/app/MainApplicationTest.java
@@ -6,6 +6,7 @@ public class MainApplicationTest {
   
   @Test
   public void testSpringStartsCorrectly(){
+    System.setProperty("server.port", "0"); //grab a random port so it doesn't interfere with jenkins
     MainApplication.main(new String[] {});
   }
 }
